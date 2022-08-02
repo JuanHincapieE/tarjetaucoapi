a7dfdf34a325b0dff083789e95492ac206d7486b
Julián Vásquez <julidoo06@gmail.com>
2022-08-01 22:33:24 -0500

    Proyect inicializado

---
+++ src/test/java/com/tarjetaucoapi/tarjetaucoapi/TarjetaucoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarjetaucoapi.tarjetaucoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarjetaucoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
