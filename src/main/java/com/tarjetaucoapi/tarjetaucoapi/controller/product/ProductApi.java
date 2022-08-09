fa54a1a556141d2ba748998e06c9a41fe8d994d2
DESKTOP-GODNBNC\Sebastian <juan.hincapie9893@uco.net.co>
2022-08-08 22:11:00 -0500

    Modificación productController

--- src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/product/ProductApi.java
+++
@@ -1,4 +0,0 @@
-package com.tarjetaucoapi.tarjetaucoapi.controller.product;
-
-public class ProductApi {
-}
---
+++ src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/product/ProductController.java
@@ -0,0 +1,16 @@
+package com.tarjetaucoapi.tarjetaucoapi.controller.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/v1/rest")
+public class ProductController {
+    @GetMapping("/products")
+    public String  getPerson(@RequestParam(required = true) String documentType, String documentNumber){
+
+        return "Estoy retornando un inventario";
+
+    }
+}
