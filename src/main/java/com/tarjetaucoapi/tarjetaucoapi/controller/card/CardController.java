898853ccddd457dde0390ca22c54b3d9cc661f3d
Julio Salas <thejuliosalas@gmail.com>
2022-08-10 22:04:39 -0500

    se agrega apiController

--- src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/card/CardController.java
+++ src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/card/CardController.java
@@ -1,19 +1,25 @@
 package com.tarjetaucoapi.tarjetaucoapi.controller.card;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/rest")
 
 public class CardController {
+
+    //, USERNAME, USERLASTNAME, CVV, EXPIRATION
     @GetMapping("/cards")
-    public String  getPerson(@RequestParam(required = true) String documentType, String documentNumber){
+    public String  getcard(@RequestParam(required = true) int CardNumber, String UserName,
+                             String UserLastName, int CVV, int Expiration){
 
         return "Estoy retornando una tarjeta a";
 
+    }
+    @PostMapping("/cards")
+    public String postcard(@RequestParam(required = true) int CardNumber, String UserName,
+                             String UserLastName, int CVV, int Expiration){
+
+        return "estoy creando una tarjeta";
     }
 }
