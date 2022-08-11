d2f84ef564463ede0b21bfeb714989f7fb98eeb6
Julián Vásquez <julidoo06@gmail.com>
2022-08-10 21:50:38 -0500

    Se agrega post de inventory

--- src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/inventory/InventoryController.java
+++ src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/inventory/InventoryController.java
@@ -1,10 +1,7 @@
 package com.tarjetaucoapi.tarjetaucoapi.controller.inventory;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/rest")
@@ -12,9 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class InventoryController {
 
     @GetMapping("/inventories")
-    public String  getPerson(@RequestParam(required = true) String documentType, String documentNumber){
-
+    public String  getPerson(@RequestParam(required = true) String idProduct, String productName, String productType, String idStore, String storeName) {
         return "Estoy retornando un inventario";
+    }
 
+    @PostMapping("/inventories")
+    public String  pstPerson(@RequestParam(required = true) String idProduct, String productName, String productType, String idStore, String storeName){
+        return "Estoy creando un inventario";
     }
 }
+
+
--- src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/store/StoreController.java
+++ src/main/java/com/tarjetaucoapi/tarjetaucoapi/controller/store/StoreController.java
@@ -16,4 +16,5 @@ public class StoreController {
         return "Estoy retornando la tiendaa" + documentNumber ;
 
     }
+
 }
