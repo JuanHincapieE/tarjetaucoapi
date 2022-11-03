a6b01b311c03219a6d268a11a08064fd9679dffc
Atzuke <c.riman@hotmail.com>
2022-11-02 20:37:00 -0500

    se eliminan las clases sobrantes

--- src/main/java/com/tarjetaucoapi/tarjetaucoapi/repositories/record/IRecordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tarjetaucoapi.tarjetaucoapi.repositories.record;
-
-import com.tarjetaucoapi.tarjetaucoapi.domains.record.Record;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface IRecordRepository extends CrudRepository<Record, Integer> {
-}
